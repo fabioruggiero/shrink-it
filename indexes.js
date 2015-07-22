c07d0608ecee48cc3f1a11bb7b8dab60af6c402b
fabioruggiero <f.ruggiero90@gmail.com>
2015-07-22 05:54:40 +0200

    last commit

--- indexes.js
+++ indexes.js
@@ -12,11 +12,3 @@ eb.send(pa, {
     })
 });
 
-eb.send(pa, {
-    action: 'command',
-    command: JSON.stringify({
-        createIndexes: 'visits',
-        indexes: {key: {id_url: 1}},
-        name: "idUrl_index"
-    })
-});
